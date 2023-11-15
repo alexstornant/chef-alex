3ef886a3236c717feeb29888e06fd3521c2f665f
alexstornant <stornant.alex@gmail.com>
2023-11-15 11:09:06 -0500

    suggestion page html/css

---
+++ index.js
@@ -0,0 +1,21 @@
+// index.js
+
+function submitForm() {
+    var form = document.getElementById("recipeForm");
+
+    // Get form values
+    var name = form.elements["name"].value;
+    var email = form.elements["email"].value;
+    var recipeName = form.elements["recipeName"].value;
+    var ingredients = form.elements["ingredients"].value;
+    var directions = form.elements["directions"].value;
+    var notes = form.elements["notes"].value;
+
+    // Log form values to console
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Recipe Name:", recipeName);
+    console.log("Ingredients:", ingredients);
+    console.log("Directions:", directions);
+    console.log("Notes:", notes);
+}
