30bc131ff983f02f58f41c337759501ee7e1d146
alexstornant <stornant.alex@gmail.com>
2023-11-15 19:29:50 -0500

    suggestion page email sending

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 function submitForm() {
     var form = document.getElementById("recipeForm");
 
-    // Get form values
     var name = form.elements["name"].value;
     var email = form.elements["email"].value;
     var recipeName = form.elements["recipeName"].value;
@@ -10,11 +9,81 @@ function submitForm() {
     var directions = form.elements["directions"].value;
     var notes = form.elements["notes"].value;
 
-    // Log form values to console
-    console.log("Name:", name);
-    console.log("Email:", email);
-    console.log("Recipe Name:", recipeName);
-    console.log("Ingredients:", ingredients);
-    console.log("Directions:", directions);
-    console.log("Notes:", notes);
+    new_message = `
+    Name: ${name}
+
+    Email: ${email}
+
+    Recipe Name:
+    ${recipeName}
+
+    Ingredients:
+    ${ingredients}
+
+    Directions:
+    ${directions}
+
+    Notes:
+    ${notes}
+
+    `
+
+    var params = {
+        from_name: name,
+        message: new_message
+    };
+
+    // check if values valid
+    if (name === '' || email === '' || recipeName === '' || ingredients === '' || directions === '') {
+        showErrorMessage();
+    }
+    else {
+        form.reset();
+        sendEmail(params);
+        showSuccessMessage();
+    }
+}
+
+function showErrorMessage() {
+    var successMessage = document.getElementById('error-message');
+
+    successMessage.style.display = 'block';
+
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+    
+    setTimeout(function () {
+        successMessage.style.opacity = '0'; 
+        setTimeout(function () {
+            successMessage.style.display = 'none';
+            successMessage.style.opacity = '1';
+        }, 1000);
+    }, 2000);
+}
+
+function showSuccessMessage() {
+    var successMessage = document.getElementById('success-message');
+
+    successMessage.style.display = 'block';
+
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+    
+    setTimeout(function () {
+        successMessage.style.opacity = '0'; 
+        setTimeout(function () {
+            successMessage.style.display = 'none';
+            window.location.href = 'index.html';
+        }, 1000);
+    }, 2000);
+}
+
+function sendEmail(params) {
+    emailjs.send('service_cu97r0c','template_6n7czes', params).then(function (res) {
+        console.log("Email Status: " + res.status);
+    });
 }
